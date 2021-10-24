40d7be294b83ad0be793764383e8b6687cdb3e70
1Dice <23101388+1DIce@users.noreply.github.com>
2021-10-24 20:28:52 +0200

    New bindings: - move up/down faster - set undo breakpoints - move text up and down

--- config/nvim/lua/ld/defaults/remaps.lua
+++ config/nvim/lua/ld/defaults/remaps.lua
@@ -1,13 +1,14 @@
 -- reload
 nnoremap('<leader><CR>', '<cmd>lua reload()<CR>', 'editor', 'reload', 'Reloads configuration')
 
--- move upper/down on visual
-vnoremap('K', '<cmd>m "<-2<CR>gv=gv"', 'remap', 'remap_move_upper_on_visual', 'Move upper on visual')
-vnoremap('J', '<cmd>m ">+1<CR>gv=gv"', 'remap', 'remap_move_down_on_visual', 'Move down on visual')
+-- move up/down faster 
+nnoremap('J', '5j', 'remap', 'remap_move_down_fast_normal_mode', 'Move down faster')
+nnoremap('K', '5k', 'remap', 'remap_move_up_fast_normal_mode', 'Move up faster')
+vnoremap('J', '5j', 'remap', 'remap_move_down_fast_visual_mode', 'Move down faster visual mode')
+vnoremap('K', '5k', 'remap', 'remap_move_up_fast_visual_mode', 'Move up faster visual mode')
 
--- move up/down 
-nnoremap('J', '5j', 'remap', 'remap_move_down_fast', 'Move down faster')
-nnoremap('K', '5k', 'remap', 'remap_move_up_fast', 'Move up faster')
+-- Y should yank until end of line similar to D and C
+nnoremap('Y','y$', 'remap', 'remap_yank_to_end_of_line', 'Yank to end of line')
 
 -- deletes in visual
 vnoremap('X', '"_d', 'remap', 'remap_deletes_on_visual', 'Deletes on visual')
@@ -30,3 +31,20 @@ cnoremap <expr> <c-p> getcmdtype() =~ '[\/?]' ? '<c-t>' : '<c-p>'
 
 -- editing remaps
 nnoremap('<leader>ej', 'gJ', 'remap', 'remap_join_lines', 'join 2 lines')
+
+-- Undo break points
+inoremap(',', ',<C-g>u', 'remap', 'remap_comma_undo_break_point', 'add undo break point after ,')
+inoremap('.', '.<C-g>u', 'remap', 'remap_dot_undo_break_point', 'add undo break point after .')
+inoremap('?', '?<C-g>u', 'remap', 'remap_question_mark_undo_break_point', 'add undo break point after ?')
+inoremap('[', '[<C-g>u', 'remap', 'remap_open_bracket_undo_break_point', 'add undo break point after [')
+inoremap(']', ']<C-g>u', 'remap', 'remap_close_bracket_undo_break_point', 'add undo break point after ]')
+inoremap('{', '{<C-g>u', 'remap', 'remap_open_curly bracket_undo_break_point', 'add undo break point after {')
+inoremap('}', '}<C-g>u', 'remap', 'remap_close_curly bracket_undo_break_point', 'add undo break point after }')
+
+-- Moving text up and down youtube https://youtu.be/hSHATqh8svM
+nnoremap('<A-j>', ':m .+1<CR>==', 'remap', 'remap_move_line_down_normal_mode', 'Move current line down')
+nnoremap('<A-k>', ':m .-2<CR>==', 'remap', 'remap_move_line_up_normal_mode', 'Move current line up')
+vnoremap('<A-j>', ":m '>+1<CR>gv=gv", 'remap', 'remap_move_selection_down', 'Move current selection down')
+vnoremap('<A-k>', ":m '<-2<CR>gv=gv", 'remap', 'remap_move_selection_up', 'Move current selection up')
+inoremap('<A-j>', '<esc>:m .+1<CR>==', 'remap', 'remap_move_line_down_insert_mode', 'Move current line down (Insert mode)')
+inoremap('<A-k>', '<esc>:m .-2<CR>==', 'remap', 'remap_move_line_up_insert_mode', 'Move current line up (Insert mode)')
