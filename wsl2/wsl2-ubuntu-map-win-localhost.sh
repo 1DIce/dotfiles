33e6d8c717c31ee7fe3123b16de574fcd0288c92
lars.debor <lars.debor@cas.de>
2021-10-31 19:41:47 +0100

    Map WSL2 localhost to windows script

---
+++ wsl2/wsl2-ubuntu-map-win-localhost.sh
@@ -0,0 +1,14 @@
+
+# source: https://gist.github.com/toryano0820/6ee3bff2474cdf13e70d972da710996a
+nameserver=$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}')   # find nameserver
+[ -n "$nameserver" ] || "unable to find nameserver" || exit 1            # exit immediately if nameserver was not found
+echo "##### nameserver found: '$nameserver'"
+localhost_entry=$(grep -v "127.0.0.1" /etc/hosts | grep "\slocalhost$")  # find localhost entry excluding 127.0.0.1
+if [ -n "$localhost_entry" ]; then                                       # if localhost entry was found
+  echo "##### localhost entry found: '$localhost_entry'"
+  sed -i "s/$localhost_entry/$nameserver localhost/g" /etc/hosts       # then update localhost entry with the new $nameserver
+else                                                                     # else if entry was not found
+  echo "##### localhost entry not found"
+  echo "$nameserver localhost" >> /etc/hosts                           # then append $nameserver mapping to localhost
+fi
+cat /etc/hosts                                                           # display to confirmf dpkg -s "${boostlibnames[@]}" >/dev/null 2>&1
