2f6753b1c29f7574d199a372e746be17afca830b
lars.debor <lars.debor@cas.de>
2021-11-02 16:35:54 +0100

    Vimspector config

---
+++ config/nvim/lua/ld/plugins/vimspector/settings.lua
@@ -0,0 +1,9 @@
+-- let g:vimspector_enable_mappings = 'HUMAN'nmap <leader>vl :call vimspector#Launch()<CR>
+-- nmap <leader>vr :VimspectorReset<CR>
+-- nmap <leader>ve :VimspectorEval
+-- nmap <leader>vw :VimspectorWatch
+-- nmap <leader>vo :VimspectorShowOutput
+-- nmap <leader>vi <Plug>VimspectorBalloonEval
+-- xmap <leader>vi <Plug>VimspectorBalloonEvallet g:vimspector_install_gadgets = [ 'debugpy', 'vscode-go', 'CodeLLDB', 'vscode-node-debug2' ]
+vim.g.vimspector_enable_mappings = "HUMAN"
+vim.g.vimspector_isntall_gadgets = {"vscode-js-debug"}
