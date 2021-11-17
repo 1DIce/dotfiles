11ce25521fd9386130f30b526ec1f85633ff2fca
lars.debor <lars.debor@cas.de>
2021-11-17 08:06:27 +0100

    [nvim] only rename with angular ls and not with tsserver

--- config/nvim/lua/ld/lsp/servers/tsserver.lua
+++ config/nvim/lua/ld/lsp/servers/tsserver.lua
@@ -7,6 +7,7 @@ local initOptions = {
     typescript = {format = {indentSize = 2}}
   }
 }
+
 return function(on_attach)
   return {
     on_attach = function(client, bufnr)
@@ -14,8 +15,8 @@ return function(on_attach)
 
       remaps.set_typescript(client, bufnr)
 
-      -- tsserver, stop messing with prettier da fuck!
-      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_rename = false -- renaming is done by angularls
+      client.resolved_capabilities.document_formatting = false -- tsserver, stop messing with prettier da fuck!
     end,
     init_options = initOptions
   }
