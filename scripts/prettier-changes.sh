c07965267ab59ceae120bec6a960eae13ca845b0
lars.debor <lars.debor@cas.de>
2021-11-18 17:57:04 +0100

    Extend prettier pre-commit script for quote-view project

--- scripts/prettier-changes.sh
+++ scripts/prettier-changes.sh
@@ -6,6 +6,8 @@ FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g' )
 [ -z "$FILES" ] && exit 0
 
 M_SALES_FILES=$(echo "$FILES" | grep '^m.sales.neo')
+QUOTE_VIEW_APP_FILES=$(echo "$FILES" | grep '^quote-view/quote-view.app')
+QUOTE_VIEW_E2E_FILES=$(echo "$FILES" | grep '^quote-view/quote-view.e2e')
 
 if [ ! -z "$M_SALES_FILES" ]
 then
@@ -16,6 +18,30 @@ then
   echo "$M_SALES_FILES" | xargs npx prettier --ignore-unknown --write
   # Add back the modified/prettified files to staging
   echo "$M_SALES_FILES" | xargs git add
+  popd
+fi
+
+if [ ! -z "$QUOTE_VIEW_APP_FILES" ]
+then
+  pushd "$GIT_ROOT_DIR/quote-view/quote-view.app"
+  # convert relative file paths to absolute paths
+  QUOTE_VIEW_APP_FILES=$(echo "$QUOTE_VIEW_APP_FILES" | sed "s|^|$GIT_ROOT_DIR/|g")
+  # Prettify all selected files
+  echo "$QUOTE_VIEW_APP_FILES" | xargs npx prettier --ignore-unknown --write
+  # Add back the modified/prettified files to staging
+  echo "$QUOTE_VIEW_APP_FILES" | xargs git add
+  popd
+fi
+
+if [ ! -z "$QUOTE_VIEW_E2E_FILES" ]
+then
+  pushd "$GIT_ROOT_DIR/quote-view/quote-view.e2e"
+  # convert relative file paths to absolute paths
+  QUOTE_VIEW_E2E_FILES=$(echo "$QUOTE_VIEW_E2E_FILES" | sed "s|^|$GIT_ROOT_DIR/|g")
+  # Prettify all selected files
+  echo "$QUOTE_VIEW_E2E_FILES" | xargs npx prettier --ignore-unknown --write
+  # Add back the modified/prettified files to staging
+  echo "$QUOTE_VIEW_E2E_FILES" | xargs git add
   popd
 fi
 
