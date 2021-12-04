22fe517ef6afb2529b46b195cf358f7d9adbb676
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-04 18:20:07 +0100

    [nvim] Improved treesitter and autopairs settings

--- nvim/nvim/lua/ld/plugins/nvim-autopairs/settings.lua
+++ nvim/nvim/lua/ld/plugins/nvim-autopairs/settings.lua
@@ -3,9 +3,10 @@ local present2, autopairs_completion = pcall(require, 'nvim-autopairs.completion
 
 if not (present1 or present2) then return end
 
-autopairs.setup({check_ts = true})
+autopairs.setup({check_ts = true, disable_filetype = {"TelescopePrompt", "guihua", "guihua_rust", "clap_input"}})
 
 -- If you want insert `(` after select function or method item
+
 -- local cmp = require('cmp')
 -- cmp.event:on('confirm_done', autopairs_completion.on_confirm_done({
 --   map_char = { -- modifies the function or method delimiter by filetypes
--- nvim/nvim/lua/ld/plugins/treesitter/settings.lua
+++ nvim/nvim/lua/ld/plugins/treesitter/settings.lua
@@ -4,17 +4,16 @@ local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 parser_configs.norg = {
   install_info = {url = "https://github.com/vhyrro/tree-sitter-norg", files = {"src/parser.c"}, branch = "main"}
 }
-
-parser_configs.css.used_by = "less"
 -- end neorgtreesitter
+parser_configs.css.used_by = "less"
 
 local treesitter = require 'nvim-treesitter.configs'
 
 treesitter.setup {
-  ensure_installed = {"typescript", "angular", "lua", "json", "css", "javascript", "html", "bash", "toml", "yaml"},
-  highlight = {enable = true, additional_vim_regex_highlighting = false},
+  ensure_installed = {"typescript", "lua", "json", "css", "javascript", "html", "bash", "toml", "yaml"},
+  highlight = {enable = true, additional_vim_regex_highlighting = true},
   rainbow = {enable = true, extended_mode = false, max_file_lines = nil},
   matchup = {enable = true},
-  refactor = {highlight_current_scope = {enable = false}, highlight_definitions = {enable = true}},
+  refactor = {highlight_current_scope = {enable = false}, highlight_definitions = {enable = false}},
   autotag = {enable = true}
 }
