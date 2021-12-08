92edcb15fcdbd2572df896816e512cbe950ca417
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-08 13:15:30 +0100

    [nvim] position fzf window on the bottom half of the screen

--- nvim/nvim/lua/ld/plugins/fzf/settings.lua
+++ nvim/nvim/lua/ld/plugins/fzf/settings.lua
@@ -8,9 +8,9 @@ require'fzf-lua'.setup {
     -- "aboveleft vnew   : split left
     -- Only valid when using a float window
     -- (i.e. when 'split' is not defined)
-    height = 0.85, -- window height
+    height = 0.55, -- window height
     width = 0.95, -- window width
-    row = 0.35, -- window row position (0=top, 1=bottom)
+    row = 0.88, -- window row position (0=top, 1=bottom)
     col = 0.50, -- window col position (0=left, 1=right)
     -- border argument passthrough to nvim_open_win(), also used
     -- to manually draw the border characters around the preview
