bfe1185bf2bf6a1e2d1d6fc3da915842c1996fe0
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-12 17:58:39 +0100

    [nvim] Setup null-ls config
    configured cspell, black python and luaformatter

---
+++ nvim/nvim/lua/ld/lsp/servers/null-ls.lua
@@ -0,0 +1,35 @@
+local null_ls = require("null-ls")
+local lsp = require('lspconfig')
+
+null_ls.config({
+    sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.cspell.with({
+            filetypes = {},
+            disabled_filetypes = {"lua"},
+            extra_args = function(params)
+                if (lsp.util.root_pattern(".vscode/cspell.json")(params.bufname)) then
+                    return params.options and
+                               {"--config", ".vscode/cspell.json"}
+                end
+                return params.options
+            end,
+
+            cwd = function(params)
+                local cwd = lsp.util.root_pattern("tsconfig.json")(
+                                params.bufname) or
+                                lsp.util
+                                    .root_pattern(".vscode/")(params.bufname) or
+                                lsp.util
+                                    .root_pattern("package.json", ".git/",
+                                                  ".zshrc")(params.bufname) or
+                                lsp.util.path.dirname(params.bufname);
+                return cwd
+            end
+
+        }), null_ls.builtins.formatting.lua_format
+    },
+    debounce = 600
+})
+require("lspconfig")["null-ls"].setup({})
+
