9b4d521e34e192ec64e9b4fc7a8e6f7ce556a0aa
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-15 16:41:36 +0100

    WIP work on gitlab.sh

--- scripts/gitlab.sh
+++ scripts/gitlab.sh
@@ -4,14 +4,44 @@ set -e
 draft() {
   # check if remote is tracked?
 
-  MR_EXISTS=
+  MR_EXISTS=true
+  TRACKED_BRANCH_EXISTS=true
+  TRACKED_BRANCH =$(git rev-parse --symbolic-full-name --abbrev-ref  HEAD@{u} | sed -e "s/^origin\///")
+  if [ $? -ne 0]; then
+    echo "Remote branch does not exist yet"
+    MR_EXISTS=false
+    TRACKED_BRANCH_EXISTS=false
+  else
+    MR_LIST=$(glab mr  list --source-branch="$TRACKED_BRANCH")
+    if [$? -ne 0]; then
+      echo "No merge request found for the tracked branch"
+      MR_EXISTS=false
+    else
+      NUMBER_OF_MR=$(echo "$MR_LIST" | wc -l)
+      if [$NUMBER_OF_MR -ne 1]; then
+        echo "Error: More then one related Merge request was found"
+        exit 1;
+      fi
+      MR_EXISTS=true
+  fi
 
-  TARGET_BRANCH= echo "master\nlts/10.0\n/lts/10.10" | fzf
+
+  if [$MR_EXISTS]; then 
+    glab mr update --draft
+    # TODO i need the --force flag for push
+    git push
+  else
+    if[$TRACKED_BRANCH_EXISTS -e false]; then
+      LOCAL_BRANCH_NAME=$(git rev-parse --symbolic-full-name --abbrev-ref  HEAD)
+      git push --set-upstream origin "$LOCAL_BRANCH_NAME"
+    fi
+    TARGET_BRANCH=$(echo "master\nlts/10.0\n/lts/10.10" | fzf)
+    glab mr create --assignee=@me --fill --draft --label team-ruby --target-banch"$TARGET_BRANCH"
+
 
 
   # check if mr already exists?
 
-  glab mr create --assignee=@me --fill --draft --label team-ruby
 
 }
 
