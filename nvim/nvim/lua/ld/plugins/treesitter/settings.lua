750acf7d92fc9769bab3adb41b877233cf092478
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-17 11:05:26 +0100

    [nvim] Configure treesitter markdown

--- nvim/nvim/lua/ld/plugins/treesitter/settings.lua
+++ nvim/nvim/lua/ld/plugins/treesitter/settings.lua
@@ -2,7 +2,11 @@
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_configs.norg = {
-  install_info = {url = "https://github.com/vhyrro/tree-sitter-norg", files = {"src/parser.c"}, branch = "main"}
+    install_info = {
+        url = "https://github.com/vhyrro/tree-sitter-norg",
+        files = {"src/parser.c"},
+        branch = "main"
+    }
 }
 -- end neorgtreesitter
 parser_configs.css.used_by = "less"
@@ -10,10 +14,16 @@ parser_configs.css.used_by = "less"
 local treesitter = require 'nvim-treesitter.configs'
 
 treesitter.setup {
-  ensure_installed = {"typescript", "lua", "json", "css", "javascript", "html", "bash", "toml", "yaml"},
-  highlight = {enable = true, additional_vim_regex_highlighting = true},
-  rainbow = {enable = true, extended_mode = false, max_file_lines = nil},
-  matchup = {enable = true},
-  refactor = {highlight_current_scope = {enable = false}, highlight_definitions = {enable = false}},
-  autotag = {enable = true}
+    ensure_installed = {
+        "typescript", "markdown", "lua", "json", "css", "javascript", "html",
+        "bash", "toml", "yaml"
+    },
+    highlight = {enable = true, additional_vim_regex_highlighting = true},
+    rainbow = {enable = true, extended_mode = false, max_file_lines = nil},
+    matchup = {enable = true},
+    refactor = {
+        highlight_current_scope = {enable = false},
+        highlight_definitions = {enable = false}
+    },
+    autotag = {enable = true}
 }
