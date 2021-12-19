7bd751790480f6b29809b032839fed34c1389f87
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-19 15:54:56 +0100

    [nvim] load harpoon telescope extension

--- nvim/nvim/lua/ld/plugins/telescope/settings.lua
+++ nvim/nvim/lua/ld/plugins/telescope/settings.lua
@@ -3,40 +3,44 @@ local action_layout = require('telescope.actions.layout')
 local functions = require 'ld.utils.functions'
 
 telescope.setup {
-  defaults = {
-    layout_config = {prompt_position = 'top'},
-    path_display = {"smart"},
-    mappings = {
-      i = {
-        -- mappings are used while telescope prompt is open
-        ["<C-h>"] = "which_key", -- actions.which_key shows the mappings for your picker,
-        ["<C-j>"] = "move_selection_next",
-        ["<C-k>"] = "move_selection_previous",
-        ['<F4>'] = action_layout.toggle_preview
-      },
-      n = {['<F4>'] = action_layout.toggle_preview}
+    defaults = {
+        layout_config = {prompt_position = 'top'},
+        path_display = {"smart"},
+        mappings = {
+            i = {
+                -- mappings are used while telescope prompt is open
+                ["<C-h>"] = "which_key", -- actions.which_key shows the mappings for your picker,
+                ["<C-j>"] = "move_selection_next",
+                ["<C-k>"] = "move_selection_previous",
+                ['<F4>'] = action_layout.toggle_preview
+            },
+            n = {['<F4>'] = action_layout.toggle_preview}
+        },
+        sorting_strategy = "ascending",
+        timeoutlen = 2000,
+        prompt_prefix = '>',
+        file_ignore_patterns = {
+            '.backup', '.swap', '.langservers', '.session', '.undo', '*.git',
+            'node_modules', 'vendor', '.cache', '.vscode-server', '.Desktop',
+            '.Documents', 'classes'
+        },
+        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+        color_devicons = true,
+        set_env = {['COLORTERM'] = 'truecolor'} -- default = nil,
     },
-    sorting_strategy = "ascending",
-    timeoutlen = 2000,
-    prompt_prefix = '>',
-    file_ignore_patterns = {
-      '.backup', '.swap', '.langservers', '.session', '.undo', '*.git', 'node_modules', 'vendor', '.cache',
-      '.vscode-server', '.Desktop', '.Documents', 'classes'
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case" -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
+            --  ,tiebreak = {}
+        }
     },
-    borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-    color_devicons = true,
-    set_env = {['COLORTERM'] = 'truecolor'} -- default = nil,
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case" -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
-      --  ,tiebreak = {}
+    pickers = {
+        buffers = {sort_mru = true},
+        lsp_dynamic_workspace_symbols = {debounce = 300}
     }
-  },
-  pickers = {buffers = {sort_mru = true}, lsp_dynamic_workspace_symbols = {debounce = 300}}
 }
 
 functions.link_highlight('TelescopeBorder', 'GruvboxBg2', true)
@@ -46,3 +50,5 @@ functions.link_highlight('TelescopePreviewBorder', 'GruvboxBg2', true)
 
 if functions.is_linux() then telescope.load_extension('fzf') end
 telescope.load_extension('mapper')
+telescope.load_extension('harpoon')
+
