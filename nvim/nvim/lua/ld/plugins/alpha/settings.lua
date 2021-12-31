d752e28674f6dd1a531af4844b12ba769dad5c7a
1Dice <23101388+1DIce@users.noreply.github.com>
2021-12-31 11:56:03 +0100

    [nvim] Install "alpha" a greeter plugin for neovim
    - the last session is no longer automatically restored

--- nvim/nvim/lua/ld/plugins/alpha/settings.lua
+++ nvim/nvim/lua/ld/plugins/alpha/settings.lua
@@ -1,2 +1,30 @@
-require'alpha'.setup(require'alpha.themes.startify'.opts)
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+-- Set header
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     "
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button("s", "  > Restore session", ":RestoreSession <CR><CR>"),
+    dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("f", "  > Find file",
+                     ":lua require('fzf-lua').files()<CR>"),
+    dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+    dashboard.button("c", "  > Nvim config",
+                     ":cd $HOME/.config/nvim | lua require('ld.plugins.telescope.functions').search_config() <CR>"),
+    dashboard.button("q", "  > Quit NVIM", ":qa<CR>")
+}
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
 
--- nvim/nvim/lua/ld/plugins/auto-session/settings.lua
+++ nvim/nvim/lua/ld/plugins/auto-session/settings.lua
@@ -1,4 +1,8 @@
 local session = require('auto-session')
 
-session.setup({auto_session_suppress_dirs = {'~/'}, auto_save_enabled = true, auto_restore_enabled = true})
+session.setup({
+    auto_session_suppress_dirs = {'~/'},
+    auto_save_enabled = true,
+    auto_restore_enabled = false
+})
 
