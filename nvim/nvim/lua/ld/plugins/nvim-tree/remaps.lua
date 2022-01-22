5bb08fbb9ffb7282ace399b02c28e3bbdd076016
1Dice <23101388+1DIce@users.noreply.github.com>
2022-01-22 14:06:57 +0100

    [nivm] map toggle nvim-tree to <leader>t

--- nvim/nvim/lua/ld/plugins/nvim-tree/remaps.lua
+++ nvim/nvim/lua/ld/plugins/nvim-tree/remaps.lua
@@ -1,1 +1,7 @@
-nnoremap('-', "<cmd>NvimTreeToggle<CR>", 'explorer', 'explorer_toggle', 'Toggle explorer')
+nnoremap('-', "<cmd>NvimTreeToggle<CR>", 'explorer', 'explorer_toggle',
+         'Toggle explorer')
+nnoremap('<leader>t', "<cmd>NvimTreeToggle<CR>", 'explorer',
+         'explorer_toggle_leader', 'Toggle explorer')
+
+
+
