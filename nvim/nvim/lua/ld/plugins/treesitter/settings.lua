1b31291a42076439c644a20dab57aed7dd5d223b
1Dice <23101388+1DIce@users.noreply.github.com>
2022-01-23 10:39:20 +0100

    [nvim] Disable treesitter additional regex highlighting for performance

--- nvim/nvim/lua/ld/plugins/treesitter/settings.lua
+++ nvim/nvim/lua/ld/plugins/treesitter/settings.lua
@@ -1,13 +1,4 @@
--- neorg treesitter
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
-parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/vhyrro/tree-sitter-norg",
-        files = {"src/parser.c"},
-        branch = "main"
-    }
-}
 -- end neorgtreesitter
 parser_configs.css.used_by = "less"
 
@@ -18,7 +9,7 @@ treesitter.setup {
         "typescript", "markdown", "lua", "json", "css", "javascript", "html",
         "bash", "toml", "yaml"
     },
-    highlight = {enable = true, additional_vim_regex_highlighting = true},
+    highlight = {enable = true, additional_vim_regex_highlighting = false},
     rainbow = {enable = true, extended_mode = false, max_file_lines = nil},
     matchup = {enable = true},
     refactor = {
@@ -27,3 +18,5 @@ treesitter.setup {
     },
     autotag = {enable = true}
 }
+
+
