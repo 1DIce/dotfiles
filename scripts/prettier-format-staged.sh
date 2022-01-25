109d0749b00d9d2b534788d3f70280cd0ffb133c
1Dice <23101388+1DIce@users.noreply.github.com>
2022-01-25 20:52:42 +0100

    Fix prettier-format-staged script
    the cwd has to be the root of the working tree all the time

--- scripts/prettier-format-staged.sh
+++ scripts/prettier-format-staged.sh
@@ -1,25 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 
 GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 
-pushd "$GIT_ROOT_DIR/m.sales.neo"
-git-format-staged --formatter "node_modules/.bin/prettier --config $GIT_ROOT_DIR/m.sales.neo/src/.prettierrc --stdin-filepath \"{}\"" 'src/*.js' 'src/*.json' 'src/*.ts' 'src/*.less' 'src/*.css' 'src/*.html'
-# "prettier-all": "prettier --write \"src/app/**/*.{ts, json, css, less, html}\"",
-popd
+CHANGED_FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
+CHANGES_M_SALES=$(echo "$CHANGED_FILES" | grep '^m.sales.neo')
 
-QUOTE_VIEW_APP_PATH=$(echo "$GIT_ROOT_DIR/quote-view/quote-view.app")
-pushd "$QUOTE_VIEW_APP_PATH"
-git-format-staged --formatter "node_modules/.bin/prettier --config $QUOTE_VIEW_APP_PATH/.prettierrc --ignore-path $QUOTE_VIEW_APP_PATH/.prettierignore --stdin-filepath \"{}\"" '*.js' '*.json' '*.ts' '*.less' '*.css' '*.scss' '*.html'
-popd
+if [ ! -z "$CHANGES_M_SALES" ]; then
+  M_SALES_PATH=$(echo "$GIT_ROOT_DIR/m.sales.neo")
+	git-format-staged --formatter ''"$M_SALES_PATH"'/node_modules/.bin/prettier --config '"$M_SALES_PATH"'/src/.prettierrc --stdin-filepath "{}"' 'm.sales.neo/src/*.js' 'm.sales.neo/src/*.json' 'm.sales.neo/src/*.ts' 'm.sales.neo/src/*.less' 'm.sales.neo/src/*.css' 'm.sales.neo/src/*.html'
+	if [ $? -ne 0 ]; then
+		echo "Error while formatting m.sales.neo"
+		exit $?
+	fi
+fi
 
-QUOTE_VIEW_E2E_PATH=$(echo "$GIT_ROOT_DIR/quote-view/quote-view.e2e")
-pushd "$QUOTE_VIEW_E2E_PATH"
-git-format-staged --formatter "node_modules/.bin/prettier --config $QUOTE_VIEW_E2E_PATH/.prettierrc.json --ignore-path $QUOTE_VIEW_E2E_PATH/.prettierignore --stdin-filepath \"{}\"" '*.js' '*.json' '*.ts' '*.less' '*.css' '*.scss' '*.html'
-popd
+if [ ! -z $(echo "$CHANGED_FILES" | grep '^quote-view/quote-view.app') ]; then
+  QUOTE_VIEW_REL_PATH="quote-view/quote-view.app"
+	QUOTE_VIEW_APP_PATH=$(echo "$GIT_ROOT_DIR/$QUOTE_VIEW_REL_PATH")
+	git-format-staged --formatter "$QUOTE_VIEW_APP_PATH/node_modules/.bin/prettier --config $QUOTE_VIEW_APP_PATH/.prettierrc --ignore-path $QUOTE_VIEW_APP_PATH/.prettierignore --stdin-filepath \"{}\"" "$QUOTE_VIEW_REL_PATH/*.js" "$QUOTE_VIEW_REL_PATH/*.json" "$QUOTE_VIEW_REL_PATH/*.ts" "$QUOTE_VIEW_REL_PATH/*.scss" "$QUOTE_VIEW_REL_PATH/*.html"
+	if [ $? -ne 0 ]; then
+		echo "Error while formatting quote-view.app"
+		exit $?
+	fi
+fi
 
-NG_LIBS_PATH=$(echo "$GIT_ROOT_DIR/merlin-ng-libs")
-pushd "$NG_LIBS_PATH"
-git-format-staged --formatter "node_modules/.bin/prettier --config $NG_LIBS_PATH/.prettierrc --stdin-filepath \"{}\"" '*.js' '*.json' '*.ts' '*.less' '*.css' '*.scss' '*.html'
-popd
+if [ ! -z $(echo "$CHANGED_FILES" | grep '^quote-view/quote-view.e2e') ]; then
+  QUOTE_VIEW_E2E_REL_PATH="quote-view/quote-view.e2e"
+	QUOTE_VIEW_E2E_PATH=$(echo "$GIT_ROOT_DIR/$QUOTE_VIEW_E2E_REL_PATH")
+	git-format-staged --formatter "$QUOTE_VIEW_E2E_PATH/node_modules/.bin/prettier --config $QUOTE_VIEW_E2E_PATH/.prettierrc.json --ignore-path $QUOTE_VIEW_E2E_PATH/.prettierignore --stdin-filepath \"{}\"" "$QUOTE_VIEW_E2E_REL_PATH/*.js" "$QUOTE_VIEW_E2E_REL_PATH/*.json" "$QUOTE_VIEW_E2E_REL_PATH/*.ts" "$QUOTE_VIEW_E2E_REL_PATH/*.scss" "$QUOTE_VIEW_E2E_REL_PATH/*.html"
+	if [ $? -ne 0 ]; then
+		echo "Error while formatting quote-view.e2e"
+		exit $?
+	fi
+fi
 
+if [ ! -z $(echo "$CHANGED_FILES" | grep '^merlin-ng-libs') ]; then
+	NG_LIBS_PATH=$(echo "$GIT_ROOT_DIR/merlin-ng-libs")
+	git-format-staged --formatter "$NG_LIBS_PATH/node_modules/.bin/prettier --config $NG_LIBS_PATH/.prettierrc --stdin-filepath \"{}\"" "merlin-ng-libs/*.js" "merlin-ng-libs/*.json" "merlin-ng-libs/*.ts" "merlin-ng-libs/*.scss" "merlin-ng-libs/*.html"
+  if [ $? -ne 0 ]; then
+		echo "Error while formatting merlin-ng-libs"
+		exit $?
+	fi
+fi
 
