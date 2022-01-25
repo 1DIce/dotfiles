c75a6cbcc92bacbb9456ef78a3fe535921d9c968
1Dice <23101388+1DIce@users.noreply.github.com>
2022-01-25 12:57:03 +0100

    Add variable HEAD to git-review-diff.sh

--- scripts/git-review-diff.sh
+++ scripts/git-review-diff.sh
@@ -3,24 +3,23 @@
 # example options master, lts/10.10, origin/master
 BASE_BRANCH=$1
 SKIP_FROM_MERGE_BASE=$2
+HEAD_COMMIT="${3:-HEAD}"
 LOG_SIZE=500
 
 echo "base branch: $BASE_BRANCH" >&2
 echo "skip from merge-base: $SKIP_FROM_MERGE_BASE" >&2
 
-MERGE_BASE=$(git merge-base $BASE_BRANCH HEAD)
+MERGE_BASE=$(git merge-base $BASE_BRANCH $HEAD_COMMIT)
 
 GIT_LOG=$(git log --pretty=oneline -n $LOG_SIZE)
 
 MERGE_BASE_LINE_NUMBER=$(echo "$GIT_LOG" | grep -n $MERGE_BASE | cut -f1 -d:)
-
 
 COMPARE_AGAINST_LINE_NUMBER=$((MERGE_BASE_LINE_NUMBER - SKIP_FROM_MERGE_BASE))
 
 # skipping commits
 COMPARE_AGAINST=$(echo "$GIT_LOG" | sed -n ''"$COMPARE_AGAINST_LINE_NUMBER"'p' | cut -f1 -d ' ')
 
-git diff $COMPARE_AGAINST..HEAD
-
-
+git diff $COMPARE_AGAINST..$HEAD_COMMIT
+# nvim -c "DiffviewOpen $COMPARE_AGAINST..$HEAD_COMMIT"
 
