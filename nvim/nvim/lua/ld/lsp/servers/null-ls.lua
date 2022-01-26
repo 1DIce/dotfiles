8ba20e52e1b2cd393340e5b3f506609e986a6e70
1Dice <23101388+1DIce@users.noreply.github.com>
2022-01-26 07:07:09 +0100

    [nvim] indentation-width = 2 for luaformatter

--- nvim/nvim/lua/ld/lsp/servers/null-ls.lua
+++ nvim/nvim/lua/ld/lsp/servers/null-ls.lua
@@ -2,37 +2,36 @@ local null_ls = require("null-ls")
 local lsp = require('lspconfig')
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.cspell.with({
-            filetypes = {},
-            severity = 3,
-            disabled_filetypes = {"lua", "gitconfig"},
-            extra_args = function(params)
-                if (lsp.util.root_pattern(".vscode/cspell.json")(params.bufname)) then
-                    return params.options and
-                               {"--config", ".vscode/cspell.json"}
+  sources = {
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.cspell.with({
+      filetypes = {},
+      severity = 3,
+      disabled_filetypes = {"lua", "gitconfig"},
+      extra_args = function(params)
+        if (lsp.util.root_pattern(".vscode/cspell.json")(params.bufname)) then
+          return params.options and {"--config", ".vscode/cspell.json"}
 
-                end
-                return params.options
-            end,
+        end
+        return params.options
+      end,
 
-            cwd = function(params)
-                local cwd = lsp.util.root_pattern("tsconfig.json")(
-                                params.bufname) or
-                                lsp.util
-                                    .root_pattern(".vscode/")(params.bufname) or
-                                lsp.util
-                                    .root_pattern("package.json", ".git/",
-                                                  ".zshrc")(params.bufname) or
-                                lsp.util.path.dirname(params.bufname);
-                return cwd
-            end
+      cwd = function(params)
+        local cwd = lsp.util.root_pattern("tsconfig.json")(params.bufname) or
+                        lsp.util.root_pattern(".vscode/")(params.bufname) or
+                        lsp.util
+                            .root_pattern("package.json", ".git/", ".zshrc")(
+                            params.bufname) or
+                        lsp.util.path.dirname(params.bufname);
+        return cwd
+      end
 
-        }), null_ls.builtins.formatting.lua_format,
-        null_ls.builtins.formatting.shfmt
-    },
-    debounce = 600
+    }), null_ls.builtins.formatting.lua_format.with({
+      extra_args = function(params)
+        return params.options and {"--indent-width", "2"}
+      end
+    }), null_ls.builtins.formatting.shfmt
+  },
+  debounce = 600
 })
-
 
