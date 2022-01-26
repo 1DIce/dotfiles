e44121cdd5f225464d9cdc0ca3d2c479a61e9183
1Dice <23101388+1DIce@users.noreply.github.com>
2022-01-26 19:43:48 +0100

    [nvim] Improve gitconfig file type detection

--- nvim/nvim/lua/ld/defaults/events.lua
+++ nvim/nvim/lua/ld/defaults/events.lua
@@ -1,3 +1,8 @@
-addEventListener('LuaHighlight', {'TextYankPost *'}, function() require'vim.highlight'.on_yank {timeout = 500} end)
+addEventListener('LuaHighlight', {'TextYankPost *'},
+                 function() require'vim.highlight'.on_yank {timeout = 500} end)
 
 vim.cmd([[au BufRead,BufNewFile *.jenkins   setfiletype Jenkinsfile]])
+vim.cmd([[au BufRead,BufNewFile gitconfig*   setfiletype gitconfig]])
+
+
+
