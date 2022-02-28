36e9dea3cb2af7be180f2d95ec92f828098fc77a
1Dice <23101388+1DIce@users.noreply.github.com>
2022-02-28 13:20:34 +0100

    Update build-merlin-libs.sh

--- scripts/build-merlin-libs.sh
+++ scripts/build-merlin-libs.sh
@@ -1,33 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 run_update() {
-  "$gitRoot/merlin-ng-libs/build.sh" build
-  git rev-parse HEAD >> "$gitRoot/merlin-ng-libs/.lastBuild.info"
+	"$gitRoot/merlin-ng-libs/build.sh" build
+	git rev-parse HEAD >"$gitRoot/merlin-ng-libs/.lastBuild.info"
 }
 
 gitRoot=$(git rev-parse --show-toplevel)
 
-## TODO error handling if file does not exist?
-lastUpdatedForCommit=$(cat "$gitRoot/merlin-ng-libs/.lastBuild.info")
-# if not commit id exists 
+lastBuildInfoPath="$gitRoot/merlin-ng-libs/.lastBuild.info"
 
-if [ -n "$lastUpdatedForCommit" ] 
-then
-  diffSinceLastUpdate=$(git diff  --name-only "$lastUpdatedForCommit"..HEAD -- "$gitRoot/merlin-ng-libs/")
-  # if diff exists update
-  if [ -n "$diffSinceLastUpdate" ]
-  then
-    run_update
-  fi
+if [ -f "$lastBuildInfoPath" ]; then
+	lastUpdatedForCommit=$(cat "$lastBuildInfoPath")
+fi
+
+if [ -n "$lastUpdatedForCommit" ]; then
+	diffSinceLastUpdate=$(git diff --name-only "$lastUpdatedForCommit"..HEAD -- "$gitRoot/merlin-ng-libs/")
+	# if diff exists update
+	if [ -n "$diffSinceLastUpdate" ]; then
+		run_update
+	fi
 else
-  run_update
+	run_update
 fi
 
 exit 0
 
-
 # run npm i in m.sales. Is that needed?
-
-
-
-
