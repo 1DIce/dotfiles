bb8f3b437a19722a8cfac144b3849ec7f94b38ab
1Dice <23101388+1DIce@users.noreply.github.com>
2022-03-20 19:52:33 +0100

    [nvim] Open gitlab merge request file change during diffview

--- nvim/nvim/lua/ld/defaults/commands.lua
+++ nvim/nvim/lua/ld/defaults/commands.lua
@@ -1,2 +1,15 @@
 vim.cmd(
     [[:command NgTestCur :Tkill | :T npm run test -- --include $(realpath --relative-to . %:p)]])
+
+vim.cmd(
+    [[:command DiffviewMrChange :lua require("ld.defaults.command).getDiffviewOriginalFilePath()]])
+local M = {}
+M.getDiffviewOriginalFilePath = function()
+  local absolutePath = require("diffview.lib"):get_current_view()
+                           :infer_cur_file().absolute_path
+  if absolutePath then
+    os.execute("gitlab-cli file-change " .. absolutePath .. " | xargs xdg-open")
+  end
+end
+
+return M
