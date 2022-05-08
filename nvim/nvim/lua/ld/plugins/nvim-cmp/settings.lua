90a59b0ec6f683963cb502129d32794f54c27402
1Dice <23101388+1DIce@users.noreply.github.com>
2022-05-08 07:37:33 +0200

    [nvim] Fix cmp + lspkind setup

--- nvim/nvim/lua/ld/plugins/nvim-cmp/settings.lua
+++ nvim/nvim/lua/ld/plugins/nvim-cmp/settings.lua
@@ -18,47 +18,22 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
     })
-    -- ['<Tab>'] = function(fallback)
-    --     if cmp.visible() then
-    --         cmp.select_next_item()
-    --     elseif require('luasnip').expand_or_jumpable() then
-    --         vim.fn.feedkeys(vim.api.nvim_replace_termcodes(
-    --                             '<Plug>luasnip-expand-or-jump', true, true,
-    --                             true), '')
-    --     else
-    --         fallback()
-    --     end
-    -- end,
-    -- ['<S-Tab>'] = function(fallback)
-    --     if cmp.visible() then
-    --         cmp.select_prev_item()
-    --     elseif require('luasnip').jumpable(-1) then
-    --         vim.fn.feedkeys(vim.api.nvim_replace_termcodes(
-    --                             '<Plug>luasnip-jump-prev', true, true, true),
-    --                         '')
-    --     else
-    --         fallback()
-    --     end
-    -- end
   },
   formatting = {
-    format = function(entry, vim_item)
-      -- fancy icons and a name of kind
-      vim_item.kind =
-          require('lspkind').presets.default[vim_item.kind] .. ' ' ..
-              vim_item.kind
+    format = require("lspkind").cmp_format({
+      mode = "symbol",
+      before = function(entry, vim_item)
+        vim_item.menu = ({
+          nvim_lsp = '[LSP]',
+          luasnip = '[Snp]',
+          buffer = '[Buf]',
+          nvim_lua = '[Lua]',
+          path = '[Path]'
+        })[entry.source.name]
 
-      -- set a name for each source
-      vim_item.menu = ({
-        nvim_lsp = '[LSP]',
-        luasnip = '[Snp]',
-        buffer = '[Buf]',
-        nvim_lua = '[Lua]',
-        path = '[Path]'
-      })[entry.source.name]
-
-      return vim_item
-    end
+        return vim_item
+      end
+    })
   },
   sources = {
     {name = 'nvim_lsp', max_item_count = 10},
@@ -85,5 +60,3 @@ cmp.setup.cmdline(':', {
     }
   })
 })
-
-
