a6bd1a92c8ba29551dc12b27f2e60b8264d32ffe
1Dice <23101388+1DIce@users.noreply.github.com>
2022-05-08 19:15:50 +0200

    [nvim] Lua-format should use local .lua-format config file

--- nvim/nvim/lua/ld/lsp/servers/null-ls.lua
+++ nvim/nvim/lua/ld/lsp/servers/null-ls.lua
@@ -1,5 +1,5 @@
 local null_ls = require("null-ls")
-local lsp = require('lspconfig')
+local lsp = require("lspconfig")
 
 null_ls.setup({
   on_attach = function(client, bufnr)
@@ -24,18 +24,15 @@ null_ls.setup({
                         lsp.util.root_pattern(".vscode/")(params.bufname) or
                         lsp.util
                             .root_pattern("package.json", ".git/", ".zshrc")(
-                            params.bufname) or
-                        lsp.util.path.dirname(params.bufname);
+                params.bufname) or lsp.util.path.dirname(params.bufname);
         return cwd
-      end
+      end,
 
     }), null_ls.builtins.formatting.lua_format.with({
-      extra_args = function(params)
-        return params.options and {"--indent-width", "2"}
-      end
+      cwd = function(params) return lsp.util.path.dirname(params.bufname) end,
     }), null_ls.builtins.formatting.shfmt,
     null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.code_actions.shellcheck
+    null_ls.builtins.code_actions.shellcheck,
   },
-  debounce = 600
+  debounce = 600,
 })
