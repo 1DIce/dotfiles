2bfd05f942bc15c0c9f308466a91ea383106f33e
1Dice <23101388+1DIce@users.noreply.github.com>
2022-05-08 12:09:48 +0200

    [nvim] Move nvim-autopairs/settings.json to nvim-autopairs.lua

--- nvim/nvim/lua/ld/plugins/nvim-autopairs.lua
+++ nvim/nvim/lua/ld/plugins/nvim-autopairs.lua
@@ -8,13 +8,3 @@ autopairs.setup({
   check_ts = true,
   disable_filetype = {"TelescopePrompt", "guihua", "guihua_rust", "clap_input"}
 })
-
--- If you want insert `(` after select function or method item
-
--- local cmp = require('cmp')
--- cmp.event:on('confirm_done', autopairs_completion.on_confirm_done({
---   map_char = { -- modifies the function or method delimiter by filetypes
---     all = '('
---     tex = '{'
---   }
--- }))
