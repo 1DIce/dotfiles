efe4b3d273dd4a8ace7214e52940ea5d72611091
1Dice <23101388+1DIce@users.noreply.github.com>
2022-06-19 17:13:12 +0200

    Update gitconfig to work on windows

---
+++ scripts/kdiff-merge.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Use KDIff as mergetool for git in cygwin.
+#   git config --global mergetool.kd.cmd "kdiff-merge.sh \"\$LOCAL\" \"\$REMOTE\" \"\$BASE\" \"\$MERGED\""
+#   git config --global mergetool.kd.trustExitCode false
+#   git mergetool -t kd branch1..branch2
+
+local=$1
+remote=$2
+base=$3
+merged=$4
+
+if grep -q WSL2 /proc/version; then
+  /mnt/c/Program\ Files/KDiff3/kdiff3.exe "$base" "$local" "$remote" -L1 base -L2 local -L3 remote -o "$merged"
+else
+  /C/Program\ Files/KDiff3/kdiff3.exe "$base" "$local" "$remote" -L1 base -L2 local -L3 remote -o "$merged"
+fi
