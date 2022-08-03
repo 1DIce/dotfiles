7e0b884a1fddd06f4ca16472d01f864152c0d171
1Dice <23101388+1DIce@users.noreply.github.com>
2022-08-03 05:59:07 +0200

    Use kdiff3 as the mergetool for none wsl linux

--- scripts/kdiff-merge.sh
+++ scripts/kdiff-merge.sh
@@ -9,8 +9,13 @@ remote=$2
 base=$3
 merged=$4
 
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+
 if grep -q WSL2 /proc/version; then
   /mnt/c/Program\ Files/KDiff3/kdiff3.exe "$base" "$local" "$remote" -L1 base -L2 local -L3 remote -o "$merged"
+elif [ "$UNAME" == "linux" ]; then
+  # is normal linux not wsl
+  kdiff3 "$base" "$local" "$remote" -L1 base -L2 local -L3 remote -o "$merged"
 else
   /C/Program\ Files/KDiff3/kdiff3.exe "$base" "$local" "$remote" -L1 base -L2 local -L3 remote -o "$merged"
 fi
