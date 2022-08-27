0f0223f1f83dae4feab8d196674aa17691f0a5e2
1Dice <23101388+1DIce@users.noreply.github.com>
2022-08-27 07:27:24 +0200

    [nvim] Add autopair after lsp method or function completion

--- nvim/nvim/lua/ld/plugins/nvim-autopairs.lua
+++ nvim/nvim/lua/ld/plugins/nvim-autopairs.lua
@@ -1,7 +1,8 @@
 local present1, autopairs = pcall(require, "nvim-autopairs")
-local present2, autopairs_completion = pcall(require, "nvim-autopairs.completion.cmp")
+local present2, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+local present3, cmp = pcall(require, "cmp")
 
-if not (present1 or present2) then
+if not (present1 or present2 or present3) then
   return
 end
 
@@ -10,3 +11,23 @@ autopairs.setup({
   disable_filetype = { "TelescopePrompt", "guihua", "guihua_rust", "clap_input" },
   enable_bracket_in_quote = false,
 })
+
+local handlers = require("nvim-autopairs.completion.handlers")
+
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done({
+    filetypes = {
+      -- "*" is a alias to all filetypes
+      ["*"] = {
+        ["("] = {
+          kind = {
+            cmp.lsp.CompletionItemKind.Function,
+            cmp.lsp.CompletionItemKind.Method,
+          },
+          handler = handlers["*"],
+        },
+      },
+    },
+  })
+)
