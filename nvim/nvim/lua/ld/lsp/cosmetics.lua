ecc35cceb599cc92ca4f69ab1974ecc499a183d9
1Dice <23101388+1DIce@users.noreply.github.com>
2022-09-10 18:19:35 +0200

    [nvim] Enable basic underlines in windows terminal + tmux

--- nvim/nvim/lua/ld/lsp/cosmetics.lua
+++ nvim/nvim/lua/ld/lsp/cosmetics.lua
@@ -1,4 +1,4 @@
 vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticError" })
-vim.fn.sign_define("DiagnosticSignWarning", { text = "", texthl = "DiagnosticWarn" })
-vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "DiagnosticInfo" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticHint" })
