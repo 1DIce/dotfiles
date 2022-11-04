59f425e524781e0d2284a26fbcce2a4f9a58b431
1Dice <23101388+1DIce@users.noreply.github.com>
2022-11-04 17:37:46 +0100

    [nvim] Setup global statusbar and winbar

--- nvim/nvim/lua/ld/plugins/lualine-config.lua
+++ nvim/nvim/lua/ld/plugins/lualine-config.lua
@@ -1,21 +1,36 @@
 require("lualine").setup({
-  options = { disabled_filetypes = { "NvimTree" } },
+  options = {
+    disabled_filetypes = { winbar = { "NvimTree", "fugitive", "gitcommit", "alpha", "neoterm" } },
+    globalstatus = true,
+  },
   sections = {
     lualine_a = {
-      {
-        "mode",
-        fmt = function(str)
-          return str:sub(1, 1)
-        end,
-      },
+      { "mode" },
     },
-    lualine_b = { "branch", "diagnostics" },
-    lualine_c = { "filename" },
-    lualine_x = {},
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = {},
+    lualine_x = { "progress", "location" },
     lualine_y = { "encoding", "fileformat" },
     lualine_z = { "filetype" },
   },
   inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+
+  winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+  inactive_winbar = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = { "filename" },
