ae3b5e1e883f8444b8908e5cdf89e083a0b0e364
1Dice <23101388+1DIce@users.noreply.github.com>
2023-01-29 17:51:49 +0100

    [nvim] Setup rust-tools dap via codelldb

--- nvim/nvim/lua/ld/lsp/servers/rust-lsp.lua
+++ nvim/nvim/lua/ld/lsp/servers/rust-lsp.lua
@@ -2,12 +2,20 @@ local rt = require("rust-tools")
 
 M = {}
 
+local extension_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension"
+local codelldb_path = extension_path .. "/adapter/codelldb"
+-- The path is only valid on linux!
+local liblldb_path = extension_path .. "/lldb/lib/liblldb.so"
+
 function M.setup(on_attach, capabilities)
   rt.setup({
     server = {
       on_attach = function(client, bufnr)
         on_attach(client, bufnr)
       end,
+      dap = {
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+      },
     },
   })
 end
