b385414b5213dfa0560436f09a231fbc0ca9747b
1Dice <23101388+1DIce@users.noreply.github.com>
2023-02-05 12:00:51 +0100

    Add greasemonkey keybinding to toggle gitlab tree view

--- greasemonkey/gitlab.user.js
+++ greasemonkey/gitlab.user.js
@@ -4,22 +4,55 @@
 //
 // ==/UserScript==
 
+(function () {
+  document.addEventListener(
+    "keydown",
+    function (e) {
+      const reviewdCheckbox = document.querySelector(
+        "[data-testid = fileReviewCheckbox]"
+      );
+      // pressed alt+r to click "viewed" checkbox
+      if (
+        reviewdCheckbox != null &&
+        e.keyCode == 82 &&
+        !e.shiftKey &&
+        !e.ctrlKey &&
+        e.altKey &&
+        !e.metaKey
+      ) {
+        reviewdCheckbox.click();
+      }
 
-(function(){
-document.addEventListener('keydown', function(e) {
-  const reviewdCheckbox = document.querySelector("[data-testid = fileReviewCheckbox]")
-  // pressed alt+r to click "viewed" checkbox
-  if (reviewdCheckbox != null &&  e.keyCode == 82 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-    reviewdCheckbox.click();
-  }
+      const diffFileContainer = document.querySelector(".diff-file");
+      // scroll diff-file into view after clicking j or k
+      if (
+        diffFileContainer != null &&
+        (e.keyCode == 74 || e.keyCode == 75) &&
+        !e.shiftKey &&
+        !e.ctrlKey &&
+        !e.altKey &&
+        !e.metaKey
+      ) {
+        setTimeout(() => {
+          document.querySelector(".diff-file").scrollIntoView();
+        }, 500);
+      }
 
-  const diffFileContainer = document.querySelector(".diff-file")
-  // scroll diff-file into view after clicking j or k
-  if (diffFileContainer != null && (e.keyCode == 74 || e.keyCode == 75) && !e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey) {
-    setTimeout(()=> {
-       document.querySelector(".diff-file").scrollIntoView();
-    }, 500)
-  }
-
-}, false);
+      const toggleTreeViewButton = document.querySelector(
+        "[data-qa-selector = file_tree_button]"
+      );
+      // pressed alt+t to click "viewed" checkbox
+      if (
+        toggleTreeViewButton != null &&
+        e.keyCode == 84 &&
+        !e.shiftKey &&
+        !e.ctrlKey &&
+        e.altKey &&
+        !e.metaKey
+      ) {
+        toggleTreeViewButton.click();
+      }
+    },
+    false
+  );
 })();
