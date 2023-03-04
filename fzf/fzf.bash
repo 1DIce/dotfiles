ed919578f2d6677c813ad150e2f2c85921e0af39
1Dice <23101388+1DIce@users.noreply.github.com>
2023-03-04 21:49:44 +0100

    Fix fzf install on windows

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/lars/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/lars/.fzf/bin"
+if [[ ! "$PATH" == */.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/lars/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/lars/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
