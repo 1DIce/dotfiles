a87ccc5429ebbefa2740f45cf7d1fb46cc679a99
1Dice <23101388+1DIce@users.noreply.github.com>
2023-03-30 13:06:05 +0200

    Remove broken scroll diff into view from gitlab greasemonkey script

--- greasemonkey/gitlab.user.js
+++ greasemonkey/gitlab.user.js
@@ -23,28 +23,13 @@
         reviewdCheckbox.click();
       }
 
-      const diffFileContainer = document.querySelector(".diff-file");
-      // scroll diff-file into view after clicking j or k
-      if (
-        diffFileContainer != null &&
-        (e.keyCode == 74 || e.keyCode == 75) &&
-        !e.shiftKey &&
-        !e.ctrlKey &&
-        !e.altKey &&
-        !e.metaKey
-      ) {
-        setTimeout(() => {
-          document.querySelector(".diff-file").scrollIntoView();
-        }, 500);
-      }
-
       const toggleTreeViewButton = document.querySelector(
         "[data-qa-selector = file_tree_button]"
       );
       // pressed alt+t to click "viewed" checkbox
       if (
         toggleTreeViewButton != null &&
-        e.keyCode == 84 &&
+        e.keyCode == 87 &&
         !e.shiftKey &&
         !e.ctrlKey &&
         e.altKey &&
