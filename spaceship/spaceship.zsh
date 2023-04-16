ba688727ab8ab5cc08c8b1912c74beec9306f890
1Dice <23101388+1DIce@users.noreply.github.com>
2023-04-16 16:08:44 +0200

    Update spaceship prompt config

--- spaceship/spaceship.zsh
+++ spaceship/spaceship.zsh
@@ -1,2 +1,4 @@
 SPACESHIP_TIME_SHOW=true
 SPACESHIP_PACKAGE_SHOW=false
+
+spaceship remove perl scala kotlin lua dart docker_compose azure ocaml vlang zig purescript erlang ansible pulumi nix_shell gnu_screen sudo
