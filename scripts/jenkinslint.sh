88bd0a15f88df0f3ff317721b70344766bcfe883
1Dice <23101388+1DIce@users.noreply.github.com>
2023-04-25 07:05:50 +0200

    Add description to jenkinslint.sh

--- scripts/jenkinslint.sh
+++ scripts/jenkinslint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Sends the given file to the Jenkins server for validation.
+#
+# Requires environment variables:
+# JENKINS_USERNAME with the jenkins username
+# JENKINS_BASE_URL with the jenkins base url. Example https://jenkins.my-company.de
+
 readonly JENKINS_FILE_PATH=$1
 # curl will prompt for the jenkins password of the user
 curl --insecure -su "$JENKINS_USERNAME" -X POST -F "jenkinsfile=<$JENKINS_FILE_PATH" "$JENKINS_BASE_URL"/pipeline-model-converter/validate
