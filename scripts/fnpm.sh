ff60b463f0b14a278180082a2ffd080789e455eb
1Dice <23101388+1DIce@users.noreply.github.com>
2023-10-25 15:56:42 +0200

    Add script to search npm run scripts with fzf

---
+++ scripts/fnpm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Fuzzy search and run "npm run" scripts
+
+if cat package.json > /dev/null 2>&1; then
+    scripts=$(cat package.json | jq .scripts | sed '1d;$d' | fzf --height 40%)
+
+    if [[ -n $scripts ]]; then
+        script_name=$(echo "$scripts" | awk -F ': ' '{gsub(/"/, "", $1); print $1}')
+        npm run "$script_name"
+    else
+        echo "Exit: You haven't selected any script"
+    fi
+else
+    echo "Error: There's no package.json"
+fi
