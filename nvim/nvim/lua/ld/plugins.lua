d70512d9194d43484319a983cef49a5a568c182f
1Dice <23101388+1DIce@users.noreply.github.com>
2024-03-30 08:18:53 +0100

    [nvim] migrate from leap to flash.nvim

--- nvim/nvim/lua/ld/plugins.lua
+++ nvim/nvim/lua/ld/plugins.lua
@@ -131,25 +131,32 @@ return require("lazy").setup({
     end,
   },
 
-  -- use({
-  --   "ggandor/lightspeed.nvim",
-  --   config = function()
-  --     nmap("s", "<Plug>Lightspeed_omni_s", "Use lightspeed on whole window")
-  --   end,
-  -- })
   {
-    "rhysd/clever-f.vim",
-    config = function()
-      vim.keymap.set("n", ";", "<Plug>(clever-f-repeat-forward)", { remap = true })
-      vim.keymap.set("n", ",", "<Plug>(clever-f-repeat-back)", { remap = true })
-    end,
-  },
-
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      require("ld.plugins.leap")
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      search = {
+        multi_window = false,
+      },
+      label = {
+        style = "inline",
+        before = true,
+        after = false,
+      },
+      modes = {
+        char = {
+          highlight = { backdrop = false },
+        },
+      },
+    },
+  -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
 
   "tpope/vim-repeat", -- extends . repeat, for example for make it work with vim-sneak
--- nvim/nvim/lua/ld/plugins/leap.lua
+++
@@ -1,9 +0,0 @@
-local leap = require("leap")
-
-leap.setup({ case_sensitive = true })
-leap.add_default_mappings()
-nmap(
-  "s",
-  ":lua require('leap').leap { target_windows = { vim.fn.win_getid() } }<CR>",
-  "Use leap on whole window"
-)
