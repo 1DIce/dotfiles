5df2375afa89b9a9f79ade7e47c3bb146b6cc690
1Dice <23101388+1DIce@users.noreply.github.com>
2024-05-19 11:01:06 +0200

    [nvim] Move neogen config to lazy

--- nvim/nvim/lua/ld/lazy/neogen-comment.lua
+++ nvim/nvim/lua/ld/lazy/neogen-comment.lua
@@ -3,7 +3,23 @@ return {
     -- code comment string generation
     "danymat/neogen",
     config = function()
-      require("ld.plugins.neogen")
+      require("neogen").setup({
+        enabled = true,
+        languages = {
+          lua = {
+            template = {
+              annotation_convention = "emmylua", -- for a full list of annotation_conventions, see supported-languages below,
+            },
+          },
+          typescript = {
+            template = {
+              annotation_convention = "tsdoc",
+            },
+          },
+        },
+      })
+
+      nnoremap("<Leader>ld", ":lua require('neogen').generate()<CR>", "Generate doc string")
     end,
   },
 }
