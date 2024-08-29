dc77997b86749ee42fa5aade6999895c7e01d74e
1Dice <23101388+1DIce@users.noreply.github.com>
2024-08-29 08:08:23 +0200

    [nvim] Migrate from null-ls to none-ls
    installed new cspell plugin because none-ls does not support cspell out
    of the box

--- nvim/nvim/lua/ld/lazy/null-ls.lua
+++ nvim/nvim/lua/ld/lazy/null-ls.lua
@@ -1,10 +1,14 @@
 return {
   { "jayp0521/mason-null-ls.nvim", dependencies = "williamboman/mason.nvim" },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     config = function()
       require("ld.lsp.servers.null-ls")
     end,
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+      "davidmh/cspell.nvim",
+    },
   },
 }
--- nvim/nvim/lua/ld/lsp/servers/null-ls.lua
+++ nvim/nvim/lua/ld/lsp/servers/null-ls.lua
@@ -2,6 +2,19 @@ local path_util = require("ld.utils.path")
 local null_ls = require("null-ls")
 local json = require("ld.utils.json")
 local lsp = require("lspconfig")
+local cspell = require("cspell")
+
+local cspell_config = {
+  find_json = function(directory)
+    P(directory)
+    local files = vim.fs.find({ "cspell.json" }, { path = "./.vscode/", type = "file" })
+    if files and files[1] then
+      return files[1]
+    end
+
+    return nil
+  end,
+}
 
 null_ls.setup({
   on_attach = function(client, bufnr)
@@ -9,33 +22,31 @@ null_ls.setup({
   end,
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.cspell.with({
+    -- cspell might not work without a config file in ./.vscode/cspell.json
+    cspell.diagnostics.with({
       filetypes = {},
       method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
       diagnostics_postprocess = function(diagnostic)
         diagnostic.severity = vim.diagnostic.severity["INFO"]
       end,
       disabled_filetypes = { "NvimTree", "lua", "vim", "gitconfig", "vimwiki", "tex", "markdown" },
-      extra_args = function(params)
-        if lsp.util.root_pattern(".vscode/cspell.json")(params.bufname) then
-          return vim.list_extend(params.options, { "--config", ".vscode/cspell.json" })
-        end
-        return params.options
+      cwd = function()
+        return vim.uv.cwd()
       end,
-      -- cwd = function(params)
-      --   local cwd = lsp.util.root_pattern("tsconfig.json")(params.bufname)
-      --     or lsp.util.root_pattern(".vscode/")(params.bufname)
-      --     or lsp.util.root_pattern("package.json", ".git/", ".zshrc")(params.bufname)
-      --     or lsp.util.path.dirname(params.bufname)
-      --   return cwd
-      -- end,
+      config = cspell_config,
     }),
-    null_ls.builtins.code_actions.cspell,
+    cspell.code_actions.with({
+      disabled_filetypes = { "NvimTree", "lua", "vim", "gitconfig", "vimwiki", "tex", "markdown" },
+      cwd = function()
+        return vim.uv.cwd()
+      end,
+      config = cspell_config,
+    }),
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.code_actions.shellcheck,
+    -- null_ls.builtins.diagnostics.shellcheck,
+    -- null_ls.builtins.code_actions.shellcheck,
     null_ls.builtins.formatting.prettierd,
   },
   debounce = 600,
