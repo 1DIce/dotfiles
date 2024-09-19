56dd17195863b5c633650ee8e87a50fdf540dbbf
1Dice <23101388+1DIce@users.noreply.github.com>
2024-09-19 13:27:03 +0200

    Update gitlab greasemonkey script

--- greasemonkey/gitlab.user.js
+++ greasemonkey/gitlab.user.js
@@ -6,14 +6,14 @@
 
 (function () {
   // Make ticket numbers in the merge request title clickable
-  [...document.getElementsByClassName("title page-title")].forEach((e) => {
+  [...document.getElementsByClassName("title")].forEach((e) => {
     const ticket = e.innerHTML.match(/\[MER-\d+\]/g)[0];
     if (ticket) {
       e.innerHTML = e.innerHTML.replace(
         ticket,
         `<a target='_blank' href='https://jira.cas.de/browse/${ticket
           .replace("[", "")
-          .replace("]", "")}'>${ticket}</a>`
+          .replace("]", "")}'>${ticket}</a>`,
       );
     }
   });
@@ -22,7 +22,7 @@
     "keydown",
     function (e) {
       const reviewdCheckbox = document.querySelector(
-        "[data-testid = fileReviewCheckbox]"
+        "[data-testid = fileReviewCheckbox]",
       );
       // pressed alt+r to click "viewed" checkbox
       if (
@@ -35,22 +35,7 @@
       ) {
         reviewdCheckbox.click();
       }
-
-      const toggleTreeViewButton = document.querySelector(
-        "[data-qa-selector = file_tree_button]"
-      );
-      // pressed alt+w to toggle the code review file tree
-      if (
-        toggleTreeViewButton != null &&
-        e.keyCode == 87 &&
-        !e.shiftKey &&
-        !e.ctrlKey &&
-        e.altKey &&
-        !e.metaKey
-      ) {
-        toggleTreeViewButton.click();
-      }
     },
-    false
+    false,
   );
 })();
