c9029730682162478dcc9b6901f10a8902c1c0df
1Dice <23101388+1DIce@users.noreply.github.com>
2024-10-07 11:08:49 +0200

    [nvim] auto-session replace deprecated config names

--- nvim/nvim/lua/ld/lazy/auto-session.lua
+++ nvim/nvim/lua/ld/lazy/auto-session.lua
@@ -4,9 +4,9 @@ return {
     lazy = vim.g.started_by_firenvim,
     config = function()
       require("auto-session").setup({
-        auto_session_suppress_dirs = { "~/" },
-        auto_save_enabled = true,
-        auto_restore_enabled = false,
+        suppress_dirs = { "~/" },
+        auto_save = true,
+        auto_restore = false,
         -- The nvim-tree window is empty after restoring
         -- and also not really worth restoring anyway
         pre_save_cmds = { "NvimTreeClose" },
