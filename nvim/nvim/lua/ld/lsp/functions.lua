4d8f6909a5d9eca788ed419c3f6edfc2df3533d3
1Dice <23101388+1DIce@users.noreply.github.com>
2025-05-06 08:46:57 +0200

    [nvim] Change run_code_acton_sync to only use a specific lsp client

--- nvim/nvim/lua/ld/lsp/functions.lua
+++ nvim/nvim/lua/ld/lsp/functions.lua
@@ -83,25 +83,26 @@ end
 ---@param action table
 ---@param buf integer
 ---@param timeout_ms integer
+---@param lsp_client vim.lsp.Client
 ---@param attempt integer
-local function handle_code_action_sync(action, buf, timeout_ms, attempt)
+local function handle_code_action_sync(action, buf, timeout_ms, lsp_client, attempt)
   if attempt > 3 then
     vim.notify("Max resolve attempts reached for action " .. action.kind, vim.log.levels.WARN)
     return
   end
 
-  if action.edit then
+  if action.edit ~= nil then
     vim.lsp.util.apply_workspace_edit(action.edit, "utf-16")
-  elseif action.command then
-    vim.lsp.buf.execute_command(action.command)
+  elseif action.command ~= nil then
+    -- not sure if the arguments are correct :)
+    lsp_client:exec_cmd(action.command)
   else
     -- neovim:runtime/lua/vim/lsp/buf.lua shows how to run a code action
     -- synchronously. This section is based on that.
-    local resolve_result = vim.lsp.buf_request_sync(buf, "codeAction/resolve", action, timeout_ms)
-    if resolve_result then
-      for _, resolved_action in pairs(resolve_result) do
-        handle_code_action_sync(resolved_action.result, buf, timeout_ms, attempt + 1)
-      end
+    local resolve_response = lsp_client:request_sync("codeAction/resolve", action, timeout_ms, buf)
+    -- local resolve_result = vim.lsp.buf_request_sync(buf, "codeAction/resolve", action, timeout_ms)
+    if resolve_response.result ~= nil then
+      handle_code_action_sync(resolve_response.result, buf, timeout_ms, lsp_client, attempt + 1)
     else
       vim.notify(
         "Failed to resolve code action " .. action.kind .. " without edit or command",
@@ -111,15 +112,18 @@ local function handle_code_action_sync(action, buf, timeout_ms, attempt)
   end
 end
 
-function M.run_code_action_sync(action_name, buf)
+--- Runs a given synchronously against a specific lsp server
+---@param action_name string
+---@param buf integer
+---@param lsp_client vim.lsp.Client
+function M.run_code_action_sync(action_name, buf, lsp_client)
   local params = vim.lsp.util.make_range_params(0, "utf-16")
   params.context = { diagnostics = {}, only = { action_name } }
   local timeout = 1000 --ms
-  local result = vim.lsp.buf_request_sync(buf, "textDocument/codeAction", params, timeout)
-  for _, res in pairs(result or {}) do
-    for _, action in pairs(res.result or {}) do
-      handle_code_action_sync(action, buf, timeout, 0)
-    end
+  local response = lsp_client:request_sync("textDocument/codeAction", params, timeout, buf)
+  if response.result and #response.result > 0 then
+    local action = response.result[1]
+    handle_code_action_sync(action, buf, timeout, lsp_client, 0)
   end
 end
 
