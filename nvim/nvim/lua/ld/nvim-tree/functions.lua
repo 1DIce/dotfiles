1a348381945b4cb8b73389b58efec617dc78cc54
1Dice <23101388+1DIce@users.noreply.github.com>
2025-07-04 06:21:11 +0200

    [nvim] migrate to new nvim-tree api

--- nvim/nvim/lua/ld/nvim-tree/functions.lua
+++ nvim/nvim/lua/ld/nvim-tree/functions.lua
@@ -1,11 +1,10 @@
-local lib = require("nvim-tree.lib")
 local utils = require("nvim-tree.utils")
 local api = require("nvim-tree.api")
 
 local M = {}
 
 M.executeShellCommandOnTreeNode = function()
-  local node = lib.get_node_at_cursor()
+  local node = api.tree.get_node_under_cursor()
 
   local answer = vim.fn.input("Execute command: ", ":!cd " .. node.absolute_path .. " && ")
   utils.clear_prompt()
