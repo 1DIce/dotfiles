a5ffc1e29c059381b2c14a428131905f1d5c22e2
1Dice <23101388+1DIce@users.noreply.github.com>
2026-03-14 14:06:11 +0100

    [nvim] color theme configs

--- nvim/nvim/lua/ld/theme.lua
+++ nvim/nvim/lua/ld/theme.lua
@@ -19,4 +19,28 @@ local tokyodark = function()
   vim.cmd("colorscheme tokyodark")
 end
 
+local cyberdream = function()
+  vim.cmd("colorscheme cyberdream")
+end
+
+local function gruvbox()
+  vim.o.background = "dark" -- or "light" for light mode
+  require("gruvbox").setup({ contrast = "hard" })
+  vim.cmd([[colorscheme gruvbox]])
+end
+
+local function vscode()
+  vim.o.background = "dark" -- or "light" for light mode
+  require("vscode").setup({})
+  vim.cmd.colorscheme("vscode")
+end
+
+local function kanagawa()
+  vim.cmd("colorscheme kanagawa")
+end
+
 tokyonight()
+-- gruvbox()
+-- cyberdream()
+-- vscode()
+-- kanagawa()
