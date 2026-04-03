9616fdb2257ddf2249b5af4c1c4634ddd0433121
1Dice <23101388+1DIce@users.noreply.github.com>
2026-04-03 20:09:36 +0200

    [nvim] passthrough lsp progress to ghostty indicator

--- nvim/nvim/lua/ld/lsp/events.lua
+++ nvim/nvim/lua/ld/lsp/events.lua
@@ -69,18 +69,23 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
--- Setup fancy lsp progress. See https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md
 vim.api.nvim_create_autocmd("LspProgress", {
-  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
   callback = function(ev)
-    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-    vim.notify(vim.lsp.status(), "info", {
-      id = "lsp_progress",
-      title = "LSP Progress",
-      opts = function(notif)
-        notif.icon = ev.data.params.value.kind == "end" and " "
-          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-      end,
-    })
+    local value = ev.data.params.value or {}
+    if not value.kind then
+      return
+    end
+
+    local status = value.kind == "end" and 0 or 1
+    local percent = value.percentage or 0
+
+    local osc_seq = string.format("\27]9;4;%d;%d\a", status, percent)
+
+    if os.getenv("TMUX") then
+      osc_seq = string.format("\27Ptmux;\27%s\27\\", osc_seq)
+    end
+
+    io.stdout:write(osc_seq)
+    io.stdout:flush()
   end,
 })
